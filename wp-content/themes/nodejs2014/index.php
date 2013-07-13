a49d927ab821da5fac6450f55213d4ab9339bd1c
Michele Bertoli <michele@berto.li>
2013-07-13 17:15:23 +0200

    Front-page

--- wp-content/themes/nodejs2014/index.php
+++ wp-content/themes/nodejs2014/index.php
@@ -38,12 +38,142 @@
 			<!-- End LaunchRock Widget -->
 
 			<!-- mostri -->
-			<canvas data-processing-sources="<?php echo get_stylesheet_directory_uri() ?>/pde/monsters.pde"></canvas>
+			<canvas id="monsters-canvas" data-processing-sources="<?php echo get_stylesheet_directory_uri() ?>/pde/monsters.pde"></canvas>
 
 		</div>
 
+		<div id="header-container">
+			<header id="header">
+				<nav id="navigation">
+					<ul id="menu">
+						<li>
+							<a href="#" title="">
+								WEBdeBS
+							</a>
+						</li>
+						<li>
+							<a href="#" title="">
+								Workshop
+							</a>
+						</li>
+						<li>
+							<a href="#" title="">
+								Sponsor
+							</a>
+						</li>
+						<li>
+							<a href="#" title="">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</nav>
+				<a href="/" title="<?php bloginfo('name') ?>">
+					<img id="logo" src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.png" alt="<?php bloginfo('name') ?>">
+				</a> 	
+			</header>
+		</div>
+		<div id="main">
+			<article id="intro">
+				<header>
+					<h1 id="intro-title" class="title">
+						WEBdeBS
+					</h1>
+				</header>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eros tortor. Nunc ipsum erat, cursus et dictum vitae, bibendum at tortor. Vivamus aliquam, nibh eu feugiat lacinia, sapien leo placerat urna, at vulputate purus mi et libero. Proin vel ullamcorper massa. Donec sollicitudin fringilla nunc, ac egestas ligula ultrices nec. Proin quis tortor non erat faucibus eleifend. Ut luctus arcu nisl, id varius elit egestas at.
+				</p>
+			</article>
+			<article>
+				<p id="conf-description">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eros tortor. Nunc ipsum erat, cursus et dictum vitae, bibendum at tortor. Vivamus aliquam, nibh eu feugiat lacinia, sapien leo placerat urna, at vulputate purus mi et libero. Proin vel ullamcorper massa. Donec sollicitudin fringilla nunc, ac egestas ligula ultrices nec. Proin quis tortor non erat faucibus eleifend. Ut luctus arcu nisl, id varius elit egestas at.
+				</p>
+			</article>
+			<article class="event">
+				<div class="event-content">
+					<header>
+						<h2 class="event-date">
+							25 january 
+							<span class="event-year">
+								2014
+							</span>	
+						</h2>
+						<h1 class="event-title title">
+							III Node.js Conf
+						</h1>
+					</header>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eros tortor. Nunc ipsum erat, cursus et dictum vitae, bibendum at tortor. Vivamus aliquam, nibh eu feugiat lacinia, sapien leo placerat urna, at vulputate purus mi et libero. Proin vel ullamcorper massa. Donec sollicitudin fringilla nunc, ac egestas ligula ultrices nec. Proin quis tortor non erat faucibus eleifend. Ut luctus arcu nisl, id varius elit egestas at.
+					</p>
+				</div>
+			</article>
+			<article class="event">
+				<div class="event-content">
+					<header>
+						<h2 class="event-date">
+							25 january 
+							<span class="event-year">
+								2014
+							</span>	
+						</h2>
+						<h1 class="event-title title">
+							III Node.js Conf
+						</h1>
+					</header>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eros tortor. Nunc ipsum erat, cursus et dictum vitae, bibendum at tortor. Vivamus aliquam, nibh eu feugiat lacinia, sapien leo placerat urna, at vulputate purus mi et libero. Proin vel ullamcorper massa. Donec sollicitudin fringilla nunc, ac egestas ligula ultrices nec. Proin quis tortor non erat faucibus eleifend. Ut luctus arcu nisl, id varius elit egestas at.
+					</p>
+				</div>
+			</article>
+		</div>
+		<div id="slides">
+			<div class="slide">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+			</div>
+			<div class="slide">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+			</div>
+			<div class="slide">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/cirpo.png" alt="">
+			</div>
+		</div>
+		<div id="footer-container">
+			<footer id="footer">
+				<div id="call-for-paper">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eros tortor. Nunc ipsum erat, cursus et dictum vitae, bibendum at tortor. Vivamus aliquam, nibh eu feugiat lacinia, sapien leo placerat urna, at vulputate purus mi et libero. Proin vel ullamcorper massa. Donec sollicitudin fringilla nunc, ac egestas ligula ultrices nec. Proin quis tortor non erat faucibus eleifend. Ut luctus arcu nisl, id varius elit egestas at.
+					</p>
+					<ul id="social">
+						<li>
+							<a href="#" title="">
+								Twitter
+							</a>
+						</li>
+						<li>
+							<a href="#" title="">
+								Facebook
+							</a>
+						</li>
+					</ul>
+				</div>
+				<span>
+					Social
+				</span>
+				<span>
+					Seguici
+				</span>
+			</footer>
+		</div>
 
 		<!-- javascript -->
+		<script src="<?php echo get_stylesheet_directory_uri() ?>/vendor/prefixfree.min.js"></script>
+		<script src="<?php echo get_stylesheet_directory_uri() ?>/vendor/jquery.slides.min.js"></script>
 		<script data-main="<?php echo get_stylesheet_directory_uri() ?>/js/application.js" 
 				src="<?php echo get_stylesheet_directory_uri() ?>/vendor/require.js">
 		</script>
--- wp-content/themes/nodejs2014/js/application.js
+++ wp-content/themes/nodejs2014/js/application.js
@@ -19,4 +19,46 @@ require(['jquery','vendor/processing-1.4.1.min'], function ($) {
 			[ $canvas.attr("data-processing-sources") ]
 		);
 
+	$("#slides").slidesjs({
+		width: 1271,
+        height: 300,
+        navigation: false
+	});
+
+	var $window = $(window);
+	var headerTop = $("#header").position().top;
+	var intro = $("#intro");
+	var introHeight = intro.height();
+	var monsterHeight = 56;
+
+	$window
+		.scroll(function() {
+
+			var scrollTop = $window.scrollTop();
+
+			if (scrollTop >= headerTop) {
+
+				var position = introHeight - (scrollTop - headerTop);
+				
+				if (position >= (introHeight - monsterHeight)) {
+					setPosition(position);
+				} else {
+					setPosition(introHeight - monsterHeight);
+				}
+
+			} else {
+
+				setPosition(introHeight);
+
+			}
+
+		})
+		.scroll();
+
+	function setPosition(position) {
+		intro.css({
+			"background-position": "center " +  position + "px" 
+		});
+	}
+
 });
